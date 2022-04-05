19430f5ae474bdc8401f4a9d69e360a15a008283
joeyswafford <joey@swafford.co>
2022-04-05 17:00:14 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -3,31 +3,33 @@ import "./styles.css";
 
 function App() {
   return (
-    // Using css grid to layout calculator
-    <div className="calculator-grid">
-      {/* Output will contain two sections with the top section containing what the user previously entered and the focus will be on what the user is currently entering. */}
-      <div className="output">
-        <div className="previous-input">5,000 +</div>
-        <div className="current-input">5,000</div>
+    <div className="calculator-shell">
+      {/* Using css grid to layout calculator */}
+      <div className="calculator-grid">
+        {/* Output will contain two sections with the top section containing what the user previously entered and the focus will be on what the user is currently entering. */}
+        <div className="output">
+          <div className="previous-input">5,000 +</div>
+          <div className="current-input">5,000</div>
+        </div>
+        <button className="spans-two">AC</button>
+        <button>DEL</button>
+        <button>÷</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className="spans-two">=</button>
       </div>
-      <button className="spans-two">AC</button>
-      <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className="spans-two">=</button>
     </div>
   );
 }
