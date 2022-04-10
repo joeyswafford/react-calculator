348a1577e9417371175e122e4f47f3fbb9918c00
joeyswafford <joey@swafford.co>
2022-04-10 18:28:48 -0400

    added fontawesome and github link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaGithub } from "react-icons/fa";
 import usePrevious from "./usePrevious";
 import Button from "./components/Button";
 import "./styles.css";
@@ -59,45 +61,52 @@ const App = () => {
   };
 
   return (
-    <div className="calculator-shell">
-      {/* Using css grid to layout calculator */}
-      <div className="calculator-grid">
-        {/* Output will contain two sections with the top section containing what the user previously entered and the focus will be on the bottom section which is what the user is currently entering. */}
-        <div className="output">
-          <div className="previous-input">
-            <input type="text" value={prevInput} />
+    <div>
+      <div className="calculator-shell">
+        {/* Using css grid to layout calculator */}
+        <div className="calculator-grid">
+          {/* Output will contain two sections with the top section containing what the user previously entered and the focus will be on the bottom section which is what the user is currently entering. */}
+          <div className="output">
+            <div className="previous-input">
+              <input type="text" value={prevInput} />
+            </div>
+            <div className="current-input">
+              <input type="text" value={result} />
+            </div>
           </div>
-          <div className="current-input">
-            <input type="text" value={result} />
-          </div>
+          <Button text="AC" onClick={allClear} hasTwoColumns />
+          <Button text="C" onClick={clear} hasHighlight />
+          <Button text="/" onClick={addDigit} hasHighlight />
+          <Button text="7" onClick={addDigit} />
+          <Button text="8" onClick={addDigit} />
+          <Button text="9" onClick={addDigit} />
+          <Button text="*" onClick={addDigit} hasHighlight />
+          <Button text="4" onClick={addDigit} />
+          <Button text="5" onClick={addDigit} />
+          <Button text="6" onClick={addDigit} />
+          <Button text="+" onClick={addDigit} hasHighlight />
+          <Button text="1" onClick={addDigit} />
+          <Button text="2" onClick={addDigit} />
+          <Button text="3" onClick={addDigit} />
+          <Button text="-" onClick={addDigit} hasHighlight />
+          <Button text="." onClick={addDigit} />
+          <Button text="0" onClick={addDigit} />
+          <Button text="=" onClick={evaluate} hasTwoColumns />
+          <Button text="x²" onClick={square} hasHighlight isSecondary />
+          <Button text="x³" onClick={cube} hasHighlight isSecondary />
+          <Button text="√" onClick={squareRoot} hasHighlight isSecondary />
+          <Button text="%" onClick={calcPercantage} hasHighlight isSecondary />
         </div>
-        <Button text="AC" onClick={allClear} hasTwoColumns />
-        <Button text="C" onClick={clear} hasHighlight />
-        <Button text="/" onClick={addDigit} hasHighlight />
-        <Button text="7" onClick={addDigit} />
-        <Button text="8" onClick={addDigit} />
-        <Button text="9" onClick={addDigit} />
-        <Button text="*" onClick={addDigit} hasHighlight />
-        <Button text="4" onClick={addDigit} />
-        <Button text="5" onClick={addDigit} />
-        <Button text="6" onClick={addDigit} />
-        <Button text="+" onClick={addDigit} hasHighlight />
-        <Button text="1" onClick={addDigit} />
-        <Button text="2" onClick={addDigit} />
-        <Button text="3" onClick={addDigit} />
-        <Button text="-" onClick={addDigit} hasHighlight />
-        <Button text="." onClick={addDigit} />
-        <Button text="0" onClick={addDigit} />
-        <Button text="=" onClick={evaluate} hasTwoColumns />
-        <Button text="x²" onClick={square} hasHighlight isSecondary />
-        <Button text="x³" onClick={cube} hasHighlight isSecondary />
-        <Button text="√" onClick={squareRoot} hasHighlight isSecondary />
-        <Button text="%" onClick={calcPercantage} hasHighlight isSecondary />
       </div>
-      <div>
-        <a href="https://github.com/joeyswafford/react-calculator">
-          Github Repository
-        </a>
+      <div className="repo-container">
+        <div className="github-icon">
+          <FaGithub />
+        </div>
+        <div className="repo-link">
+          <a href="https://github.com/joeyswafford/react-calculator">
+            Github Repository
+          </a>
+        </div>
       </div>
     </div>
   );
